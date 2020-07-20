622afe01ca9b11770aacdccd906d47878b54cf18
Sachin Alexander Reddy <SAR@sachins-macbook.home>
2020-07-20 16:12:23 +0100

    20-Jul-20(2)

--- Sandbox/CIRCE_Sand_2.py
+++ Sandbox/CIRCE_Sand_2.py
@@ -10,11 +10,11 @@ path = r'/Users/SAR/Documents/2. Academia/2. UCL/PhD/CIRCE/Data In'
 for filename in glob.glob(os.path.join(path, '*.pkt')):    
     with open(os.path.join(os.getcwd(), filename), 'rb') as f:
         open_bites = list("{:08b}".format(c) for c in f.read()) #Opens as hex
-print('Total bites in:', len(open_bites))
+#ßprint('Total bites in:', len(open_bites))
 
 '''Validates the open by dividing all packets into packet size'''
 num_of_pkts = (len(open_bites)//264)
-print('Number of packets:', num_of_pkts)
+#print('Number of packets:', num_of_pkts)
 
 '''splits array into original packets'''
 z = np.array_split(open_bites, num_of_pkts)
@@ -40,43 +40,55 @@ def extract_burst_main():
         burst_group_a.append(skip_to_burst)
     #print('The first 42 bits have been removed',burst_group_a)
 
-    '''Burst 0'''
-    joined_pkts = []
+    '''Burst 0 (Working)'''
+    burst_1 = []
     for i in burst_group_a:
-        burst = i[0:192]
-        burst_join_1 = str("".join(i for i in burst))
-        relist_pkts = (list(j for j in burst_join_1))
-        joined_pkts.append(relist_pkts)
-        
-        burst_twelves = [joined_pkts[index:index+12] for index in range(0,len(joined_pkts),12)] #Rejoins as a twelve bit int
-        #burst_twelves = [burst_join_1[index:index+12] for index in range(0,len(burst_join_1),12)] #Rejoins as a twelve bit int
-    
-    print('Burst Joined Length',burst_twelves)
-    #print('First Twelve Values', burst_twelves)
-    #convert_to_int_1 = [int(i,2) for i in burst_twelves]
-    #convert_to_int_1 = [int(i,2) for i in burst_twelves]
-    for y in burst_twelves:
-        for x in y:
-            convert_to_int_1 = [int(str(x),2)]
-    print('Burst 0', convert_to_int_1)
+        burst_crop_1 = i[0:192]
+        burst_join_1 = str("".join(i for i in burst_crop_1))
+        burst_twelves_1 = [burst_join_1[index:index+12] for index in range(0,len(burst_join_1),12)]
+        burst_1.append(burst_twelves_1)
+        converted_to_int_1 = [int(j,2) for a in burst_1 for j in a]
+    #print('Burst 0 as binary', burst_1)
+    print('Burst 0 with loop:', converted_to_int_1)
 
-    '''Burst 1'''
+    '''Burst 1
     for i in burst_group_a:
         burst = i[193:384]
         burst_join_2 = str("".join(i for i in burst))
         burst_twelves_2 = [burst_join_2[index:index+12] for index in range(0,len(burst_join_2),12)]
     #print('First Twelve Values', burst_twelves_2)
     convert_to_int_2 = [int(i,2) for i in burst_twelves_2]
-    #print('Burst 1', convert_to_int_2)
+    #print('Burst 1', convert_to_int_2)'''
 
-    '''Burst 2'''
+    burst_2 = []
     for i in burst_group_a:
-        burst = i[385:576]
-        burst_join_3 = str("".join(i for i in burst))
+        burst_crop_2 = i[193:384]
+        burst_join_2 = str("".join(i for i in burst_crop_2))
+        burst_twelves_2 = [burst_join_2[index:index+12] for index in range(0,len(burst_join_2),12)]
+        burst_2.append(burst_twelves_2)
+        converted_to_int_2 = [int(j,2) for a in burst_2 for j in a]
+    #print('Burst 0 as binary', burst_2)
+    print('Burst 1 with loop:', converted_to_int_2)
+    
+    '''
+    Burst 2
+    for i in burst_group_a:
+        burst_crop_3 = i[385:576]
+        burst_join_3 = str("".join(i for i in burst_crop_3))
         burst_twelves_3 = [burst_join_3[index:index+12] for index in range(0,len(burst_join_3),12)]
     #print('First Twelve Values', burst_twelves_3)
     convert_to_int_3 = [int(i,2) for i in burst_twelves_3]
-    #print('Burst 2', convert_to_int_3)
+    #print('Burst 2 without loop', convert_to_int_3)'''
+
+    burst_3 = []
+    for i in burst_group_a:
+        burst_crop_3 = i[385:576]
+        burst_join_3 = str("".join(i for i in burst_crop_3))
+        burst_twelves_3 = [burst_join_3[index:index+12] for index in range(0,len(burst_join_3),12)]
+        burst_3.append(burst_twelves_3)
+        converted_to_int_3 = [int(j,2) for a in burst_3 for j in a]
+    #print('Burst 2 as binary', burst_3)
+    print('Burst 2 with loop:', converted_to_int_3)
 
     '''Second Group'''
     burst_group_b = []
@@ -85,39 +97,72 @@ def extract_burst_main():
         burst_group_b.append(skip_to_burst_2)
     #print('The first 42 bits have been removed',burst_group_a)
 
+    burst_4 = []
+    for i in burst_group_b:
+        burst_crop_4 = i[0:192]
+        burst_join_4 = str("".join(i for i in burst_crop_4))
+        burst_twelves_4 = [burst_join_4[index:index+12] for index in range(0,len(burst_join_4),12)]
+        burst_4.append(burst_twelves_4)
+        converted_to_int_4 = [int(j,2) for a in burst_4 for j in a]
+    #print('Burst 0_b as binary', burst_4)
+    print('Burst 0_b with loop:', converted_to_int_4)
+    
+    '''
     for i in burst_group_b:
         burst = i[0:192]
         burst_join_4 = str("".join(i for i in burst))
         burst_twelves_4 = [burst_join_4[index:index+12] for index in range(0,len(burst_join_4),12)]
     #print('First Twelve Values', burst_twelves_4)
     convert_to_int_4 = [int(i,2) for i in burst_twelves_4]
-    #print('Burst 0_b', convert_to_int_4)
+    #print('Burst 0_b without loop', convert_to_int_4)'''
 
-    '''Burst 1'''
+    burst_5 = []
+    for i in burst_group_b:
+        burst_crop_5 = i[193:384]
+        burst_join_5 = str("".join(i for i in burst_crop_5))
+        burst_twelves_5 = [burst_join_5[index:index+12] for index in range(0,len(burst_join_5),12)]
+        burst_5.append(burst_twelves_5)
+        converted_to_int_5 = [int(j,2) for a in burst_5 for j in a]
+    #print('Burst 0_b as binary', burst_5)
+    print('Burst 1_b with loop:', converted_to_int_5)
+
+    '''Burst 1
     for i in burst_group_b:
         burst = i[192:384]
         burst_join_5 = str("".join(i for i in burst))
         burst_twelves_5 = [burst_join_5[index:index+12] for index in range(0,len(burst_join_5),12)]
     #print('First Twelve Values', burst_twelves_5)
     convert_to_int_5 = [int(i,2) for i in burst_twelves_5]
-    #print('Burst 1_b', convert_to_int_5)
+    print('Burst 1_b', convert_to_int_5)'''
 
-    '''Burst 2'''
+    burst_6 = []
+    for i in burst_group_b:
+        burst_crop_6 = i[385:576]
+        burst_join_6 = str("".join(i for i in burst_crop_5))
+        burst_twelves_6 = [burst_join_6[index:index+12] for index in range(0,len(burst_join_6),12)]
+        burst_6.append(burst_twelves_6)
+        converted_to_int_6 = [int(j,2) for a in burst_6 for j in a]
+    #print('Burst 0_b as binary', burst_5)
+    print('Burst 2_b with loop:', converted_to_int_6)
+
+    '''Burst 2
     for i in burst_group_b:
         burst = i[385:576]
         burst_join_6 = str("".join(i for i in burst))
         burst_twelves_6 = [burst_join_6[index:index+12] for index in range(0,len(burst_join_6),12)]
     #print('Burst 2_b', burst_twelves_6)
     convert_to_int_6 = [int(i,2) for i in burst_twelves_6]
-    #print('Burst 2_b', convert_to_int_6)
+    print('Burst 2_b', convert_to_int_6)'''
 
 
     '''Plot the data'''
-    #sci_data = convert_to_int_1 + convert_to_int_2 + convert_to_int_3 + convert_to_int_4 + convert_to_int_5 + convert_to_int_5 
+    #x = converted_to_int_1 + converted_to_int_2 + converted_to_int_3 + converted_to_int_4 + converted_to_int_5 + converted_to_int_6
 
-    #plt.hist(sci_data, bins = 10000)
-    #plt.xlabel ('Energy')
-    #plt.ylabel ('Counts')
+    plt.scatter(x, y, c ='r')
+    plt.xlabel ('Energy')
+    plt.ylabel ('Counts')
+
+    #lt.show()
 
 extract_burst_main()
 
