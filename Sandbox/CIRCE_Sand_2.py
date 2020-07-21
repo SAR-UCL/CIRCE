baa3b9ba3e99f540a6e34f01c2b08c50f360a733
Sachin Alexander Reddy <SAR@sachins-macbook.home>
2020-07-21 11:04:29 +0100

    21-Jul-20(3)

--- Sandbox/CIRCE_Sand_2.py
+++ Sandbox/CIRCE_Sand_2.py
@@ -16,7 +16,7 @@ for filename in glob.glob(os.path.join(path, '*.pkt')):
 num_of_pkts = (len(open_bits)//264)
 print('Number of packets:', num_of_pkts)
 
-'''splits array into original packets'''
+'''Splits array into original packets'''
 z = np.array_split(open_bits, num_of_pkts)
 div_pkts = []
 for x in z:
@@ -24,60 +24,81 @@ for x in z:
     div_pkts.append(y)
 #print ('Packets without headers into arrays', div_pkts)
 
-'''splits into single bits'''
+'''Splits into single bits'''
 split_pkts = []
 for i in div_pkts:
     joined_pkts = "".join(j for j in i)
     relist_pkts = (list(j for j in joined_pkts))
     split_pkts.append(relist_pkts)
 
-def extract_burst_main():
-    
-    ''' First Group'''
-    burst_group_a = []
-    for i in split_pkts:
-        skip_to_burst = i[42:618]
-        burst_group_a.append(skip_to_burst)
+def extractBurstMain():
 
-    '''Second Group'''
-    burst_group_b = []
-    for j in split_pkts:
-        skip_to_burst_2 = (list(j[628:1204]))
-        burst_group_b.append(skip_to_burst_2)
+    def getIndexFromBurstGroup(startIndex):
+        burst_group = []
+        for i in split_pkts:
+            burst_index = i[(startIndex + 0):(startIndex + 192)]
+            burst_group.append(burst_index)
+        return burst_group
 
-    def getIntegersFromBurstGroup(burst_group, startIndex):
+    burst_0 = getIndexFromBurstGroup(42)
+    burst_1 = getIndexFromBurstGroup(234)
+    burst_2 = getIndexFromBurstGroup(426)
+    burst_0b = getIndexFromBurstGroup(628)
+    burst_1b = getIndexFromBurstGroup(820)
+    burst_2b = getIndexFromBurstGroup(1012)
+
+    def getIntegersFromBurstGroup(burst_group):
         burst = []
         for i in burst_group:
-            burst_index = i[(startIndex + 0):(startIndex + 192)]
+            burst_index = i[0:192]
             burst_join = str("".join(i for i in burst_index))
             burst_twelves = [burst_join[index:index+12] for index in range(0,len(burst_join),12)]
             burst.append(burst_twelves)
             convert_to_int = [int(i,2) for j in burst for i in j]
         return convert_to_int
 
-    burst_0 = getIntegersFromBurstGroup(burst_group_a, 0)
-    burst_1 = getIntegersFromBurstGroup(burst_group_a, 193)
-    burst_2 = getIntegersFromBurstGroup(burst_group_a, 385)
+    burst_0_int = getIntegersFromBurstGroup(burst_0)
+    burst_1_int = getIntegersFromBurstGroup(burst_1)
+    burst_2_int = getIntegersFromBurstGroup(burst_2)
+    burst_0b_int = getIntegersFromBurstGroup(burst_0b)
+    burst_1b_int = getIntegersFromBurstGroup(burst_1b)
+    burst_2b_int = getIntegersFromBurstGroup(burst_2b)
 
-    burst_0b = getIntegersFromBurstGroup(burst_group_b, 0)
-    burst_1b = getIntegersFromBurstGroup(burst_group_b, 193)
-    burst_2b = getIntegersFromBurstGroup(burst_group_b, 385)
+    print('Burst old', burst_1_int)
 
-extract_burst_main()
-
-def extract_burst_maximum():
+    def getIntegersFromBurstGroup2(startIndex):
+        burst = []
+        for i in split_pkts:
+            burst_index = i[(startIndex + 0):(startIndex + 192)]
+            burst_join = str("".join(i for i in burst_index))
+            burst_twelves = [burst_join[index:index+12] for index in range(0,len(burst_join),12)]
+            burst.append(burst_twelves)
+            convert_to_int = [int(i,2) for j in burst for i in j]
+        return convert_to_int
     
-    def getIntegersFromBurstMax(startIndex):
+    burst_0 = getIntegersFromBurstGroup2(42)
+    burst_1 = getIntegersFromBurstGroup2(234)
+    burst_2 = getIntegersFromBurstGroup2(426)
+    burst_0b = getIntegersFromBurstGroup2(628)
+    burst_1b = getIntegersFromBurstGroup2(820)
+    burst_2b = getIntegersFromBurstGroup2(1012)
+    print('Burst new', burst_test)
+
+extractBurstMain()
+
+def extractBurstMaximum():
+    
+    def getIndexFromBurstMax(startIndex):
         burst_max = []
         for i in split_pkts:
             burst_max_index = i[(startIndex + 0):(startIndex + 36)]
             burst_max.append(burst_max_index)
         return burst_max
 
-    burst_max_1 = getIntegersFromBurstMax(1214)
-    burst_max_2 = getIntegersFromBurstMax(1260)
-    burst_max_3 = getIntegersFromBurstMax(1306)
-    burst_max_4 = getIntegersFromBurstMax(1352)
+    burst_max_1 = getIndexFromBurstMax(1214)
+    burst_max_2 = getIndexFromBurstMax(1260)
+    burst_max_3 = getIndexFromBurstMax(1306)
+    burst_max_4 = getIndexFromBurstMax(1352)
     
     def getIntegersFromBurstMax(burst_group):
         burst = []
@@ -86,8 +107,8 @@ def extract_burst_maximum():
             burst_join = str("".join(i for i in burst_index))
             burst_twelves = [burst_join[index:index+12] for index in range(0,len(burst_join),12)]
             burst.append(burst_twelves)
-            convert_to_int = [int(i,2) for j in burst for i in j]
-        return convert_to_int
+            convert_to_int_max = [int(i,2) for j in burst for i in j]
+        return convert_to_int_max
 
     burst_max_int_1 = getIntegersFromBurstMax(burst_max_1)
     burst_max_int_2 = getIntegersFromBurstMax(burst_max_2)
@@ -96,66 +117,6 @@ def extract_burst_maximum():
 
     print ('Burst Maximum 1 (Function)', burst_max_int_4)
 
-    '''
-    def getIntegersFromBurstMax22(burst_group, startIndex):
-        burst = []
-        for i in burst_group:
-            burst_index = i[(startIndex + 0):(startIndex + 36)]
-            burst_join = str("".join(i for i in burst_index))
-            burst_twelves = [burst_join[index:index+12] for index in range(0,len(burst_join),12)]
-            burst.append(burst_twelves)
-            convert_to_int = [int(i,2) for j in burst for i in j]
-        return convert_to_int
-    '''
-
-    burst_maximum_1 = []
-    burst_maximum_2 = []
-    burst_maximum_3 = []
-    burst_maximum_4 = []
-    
-    for i in split_pkts:
-        skip_to_burst = (list(i[1214:1250]))
-        skip_to_burst_2 = (list(i[1260:1296]))
-        skip_to_burst_3 = (list(i[1306:1342]))
-        skip_to_burst_4 = (list(i[1352:1388]))
-        
-        burst_maximum_1.append(skip_to_burst)
-        burst_maximum_2.append(skip_to_burst_2)
-        burst_maximum_3.append(skip_to_burst_3)
-        burst_maximum_4.append(skip_to_burst_4)
-
-    for i in burst_maximum_1:
-        burst = i[0:36]
-        burst_join = str("".join(i for i in burst))
-        burst_twelves = [burst_join[index:index+12] for index in range(0,len(burst_join),12)] #Rejoins as a twelve bit int
-    #print('Binary Burst Maximum 1', burst_twelves)
-    convert_to_int = [int(i,2) for i in burst_twelves]
-    print('Burst Maximum 1', convert_to_int)
-
-    for i in burst_maximum_2:
-        burst = i[0:36]
-        burst_join_2 = str("".join(i for i in burst))
-        burst_twelves_2 = [burst_join_2[index:index+12] for index in range(0,len(burst_join_2),12)] #Rejoins as a twelve bit int
-    #print('Binary Burst Maximum 2', burst_twelves_2)
-    convert_to_int_2 = [int(i,2) for i in burst_twelves_2]
-    print('Burst Maximum 2', convert_to_int_2)
-
-    for i in burst_maximum_3:
-        burst = i[0:36]
-        burst_join_3 = str("".join(i for i in burst))
-        burst_twelves_3 = [burst_join_3[index:index+12] for index in range(0,len(burst_join_3),12)] #Rejoins as a twelve bit int
-    #print('Binary Burst Maximum 3', burst_twelves_3)
-    convert_to_int_3 = [int(i,2) for i in burst_twelves_3]
-    print('Burst Maximum 3', convert_to_int_3)
-
-    for i in burst_maximum_4:
-        burst = i[0:36]
-        burst_join_4 = str("".join(i for i in burst))
-        burst_twelves_4 = [burst_join_4[index:index+12] for index in range(0,len(burst_join_4),12)] #Rejoins as a twelve bit int
-    #print('Binary Burst Maximum 4', burst_twelves_4)
-    convert_to_int_4 = [int(i,2) for i in burst_twelves_4]
-    print('Burst Maximum 4', convert_to_int_4)
-
-extract_burst_maximum()
+extractBurstMaximum()
 
 
